f1be05227cc27196cd24ea4c5561f87aa40f6f7e
anshjain16 <anshjain0216@gmail.com>
2024-03-21 02:58:28 +0530

    page 1 integrated with backend server and code execution completed

--- tuf-frontend/src/App.js
+++ tuf-frontend/src/App.js
@@ -1,11 +1,17 @@
 import { Form, Input, Cascader } from "antd";
-import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
+import Editor from "@monaco-editor/react";
+import { encode as base64_encode, decode } from "base-64";
+import axios from "axios";
 import "./App.css";
 import TextArea from "antd/es/input/TextArea";
 import { useState } from "react";
 import { Button } from "antd/es/radio";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [language, setLanguage] = useState("cpp");
+  const [code, setCode] = useState("");
+  const [stdin, setStdin] = useState("");
   const languageOptions = [
     {
       value: "cpp",
@@ -30,8 +36,8 @@ function App() {
   ];
 
   const onLanguageChange = (value) => {
-    setLanguage(value);
-    console.log(value);
+    setLanguage(value[0]);
+    console.log(value[0]);
   };
 
   const handleEditorChange = (value) => {
@@ -46,14 +52,58 @@ function App() {
     setStdin(e.target.value);
   };
 
-  const handleFormSubmit = () => {
-    console.log("working");
-  };
+  const handleFormSubmit = async () => {
+    const base64code = base64_encode(code);
+    console.log(language);
+    let languageCode = languageOptions.find((l) => l.value === language).id;
+    console.log(languageCode);
 
-  const [username, setUsername] = useState("");
-  const [language, setLanguage] = useState("cpp");
-  const [code, setCode] = useState("");
-  const [stdin, setStdin] = useState("");
+    const options = {
+      method: "POST",
+      url: "https://judge0-ce.p.rapidapi.com/submissions",
+      params: {
+        base64_encoded: "true",
+        wait: "true",
+      },
+      headers: {
+        "Content-Type": "application/json",
+        "X-RapidAPI-Key": "a153a9a044mshceeb2d2e39060a2p151ab3jsn4eb3661acc00",
+        "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
+      },
+      data: {
+        language_id: languageCode,
+        source_code: base64code,
+        stdin: base64_encode(stdin),
+      },
+    };
+
+    // try {
+    //   const res1 = await axios.request(options);
+    //   console.log(res1.data);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+
+    const tempOut = "aGVsbG8gV29ybGQ=\n";
+    const res2 = await fetch(
+      `https://tuf-nfng.onrender.com/api/v1/submission`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          source_code: base64code,
+          stdin: stdin,
+          language: language,
+          output: decode(tempOut),
+          code_snippet: code.slice(0, 97) + "...",
+        }),
+      }
+    );
+    console.log(res2);
+  };
 
   return (
     <div className="App">
