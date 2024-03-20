ac6b0c61e906d0774f86ffb6dc4e73463df8041a
anshjain16 <anshjain0216@gmail.com>
2024-03-21 01:51:14 +0530

    backend hosting ready

--- tuf-backend/index.js
+++ tuf-backend/index.js
@@ -1,17 +1,23 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const dbclient = require("./dbconfig/database");
 const submissionsRouter = require("./router/submissions");
 const server = express();
 
+server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 server.use(express.json({ type: "application/vnd.api+json" }));
+server.use(cors());
+
 server.get("/", (req, res) => {
   res.send("Hello Striver");
 });
 
 server.use("/api/v1/submission", submissionsRouter);
 
-server.listen(8000, async () => {
+const port = process.env.PORT || 8080;
+server.listen(port, async () => {
   await dbclient.connect();
-  console.log("server is running on port 8000");
+  console.log(`server is running on port ${port}`);
 });
