35282e29122988407e1e99ccdc16cef292b28d43
anshjain16 <anshjain0216@gmail.com>
2024-03-21 08:27:55 +0530

    configured frontend

--- tuf-frontend/src/App.js
+++ tuf-frontend/src/App.js
@@ -1,14 +1,6 @@
-import { Form, Input, Cascader } from "antd";
-import Editor from "@monaco-editor/react";
-import { encode as base64_encode, decode } from "base-64";
-import axios from "axios";
 import Page2 from "./Page2";
 import "./App.css";
-import TextArea from "antd/es/input/TextArea";
-import { useState } from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Button } from "antd/es/radio";
 import Page1 from "./Page1";
 
 function App() {
